73ffb70077f329399778f0cf7b1331815decd0d4
Oleg Novosad <onovosad@imprivata.com>
2016-01-23 13:51:37 +0200

    Major cleanup of assets used.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,19 @@ using System.Collections.Generic;
 public class GameManager : MonoBehaviour 
 {
 	List<Room> currentRooms = new List<Room>();
+	public static GameManager instance = null;
+
+	void Awake()
+	{
+		if (instance != null && instance != this)
+		{
+			Destroy(gameObject);
+		}
+
+		instance = this;
+
+		DontDestroyOnLoad(gameObject);
+	}
 
 	// Use this for initialization
 	void Start()
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;	//Allows us to use UI.
+
+//Player inherits from MovingObject, our base class for objects that can move, Enemy also inherits from this.
+public class Player: MonoBehaviour
+{
+	public float restartLevelDelay = 1f;		//Delay time in seconds to restart level.
+	public Text foodText;						//UI Text to display current player food total.
+	public AudioClip moveSound1;				//1 of 2 Audio clips to play when player moves.
+	public AudioClip moveSound2;				//2 of 2 Audio clips to play when player moves.
+	public AudioClip eatSound1;					//1 of 2 Audio clips to play when player collects a food object.
+	public AudioClip eatSound2;					//2 of 2 Audio clips to play when player collects a food object.
+	public AudioClip drinkSound1;				//1 of 2 Audio clips to play when player collects a soda object.
+	public AudioClip drinkSound2;				//2 of 2 Audio clips to play when player collects a soda object.
+	public AudioClip gameOverSound;				//Audio clip to play when player dies.
+	
+	private Animator animator;					//Used to store a reference to the Player's animator component.
+	private Vector2 touchOrigin = -Vector2.one;	//Used to store location of screen touch origin for mobile controls.
+	
+	
+	//Start overrides the Start function of MovingObject
+	void Start ()
+	{
+		//Get a component reference to the Player's animator component
+		animator = GetComponent<Animator>();
+	}
+
+	private void Update ()
+	{
+		int horizontal = 0;  	//Used to store the horizontal move direction.
+		int vertical = 0;		//Used to store the vertical move direction.
+		
+		//Check if we are running either in the Unity editor or in a standalone build.
+		#if UNITY_STANDALONE || UNITY_WEBPLAYER
+		
+		//Get input from the input manager, round it to an integer and store in horizontal to set x axis move direction
+		horizontal = (int) (Input.GetAxisRaw ("Horizontal"));
+		
+		//Get input from the input manager, round it to an integer and store in vertical to set y axis move direction
+		vertical = (int) (Input.GetAxisRaw ("Vertical"));
+		
+		//Check if moving horizontally, if so set vertical to zero.
+		if(horizontal != 0)
+		{
+			vertical = 0;
+		}
+		
+		#endif //End of mobile platform dependendent compilation section started above with #elif
+		//Check if we have a non-zero value for horizontal or vertical
+		if(horizontal != 0 || vertical != 0)
+		{
+			// Add movemevent here
+		}
+	}
+
+	//OnTriggerEnter2D is sent when another object enters a trigger collider attached to this object (2D physics only).
+	private void OnTriggerEnter2D (Collider2D other)
+	{
+		//Check if the tag of the trigger collided with is Exit.
+		if(other.tag == "Exit")
+		{
+			//Invoke the Restart function to start the next level with a delay of restartLevelDelay (default 1 second).
+			Invoke ("Restart", restartLevelDelay);
+			
+			//Disable the player object since level is over.
+			enabled = false;
+		}
+	}
+	
+	
+	//Restart reloads the scene when called.
+	private void Restart ()
+	{
+		//Load the last scene loaded, in this case Main, the only scene in the game.
+		Application.LoadLevel (Application.loadedLevel);
+	}
+}
