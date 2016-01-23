a13fd8d790ea5dde48b9cd43ef34dfeb1bc8ce13
Oleg Novosad <onovosad@imprivata.com>
2016-01-23 13:25:20 +0200

    Added room prefab

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour 
+{
+	List<Room> currentRooms = new List<Room>();
+
+	// Use this for initialization
+	void Start()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+	
+	}
+
+	void OnLevelWasLoaded(int level)
+	{
+		GameObject[] rooms = GameObject.FindGameObjectsWithTag("Room");
+		foreach (Room room in rooms)
+		{
+			currentRooms.Add(room);
+		}
+	}
+}
