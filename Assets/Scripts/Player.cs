6028180da078ff3dd63462d37dfddb2a440b8b48
Oleg Novosad <onovosad@imprivata.com>
2016-01-23 20:33:36 +0200

    Added player movemvent.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,12 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.UI;	//Allows us to use UI.
 
-//Player inherits from MovingObject, our base class for objects that can move, Enemy also inherits from this.
-public class Player: MonoBehaviour
-{
+public class Player : MonoBehaviour {
+
 	public float restartLevelDelay = 1f;		//Delay time in seconds to restart level.
-	public Text foodText;						//UI Text to display current player food total.
 	public AudioClip moveSound1;				//1 of 2 Audio clips to play when player moves.
 	public AudioClip moveSound2;				//2 of 2 Audio clips to play when player moves.
 	public AudioClip eatSound1;					//1 of 2 Audio clips to play when player collects a food object.
@@ -14,65 +11,123 @@ public class Player: MonoBehaviour
 	public AudioClip drinkSound1;				//1 of 2 Audio clips to play when player collects a soda object.
 	public AudioClip drinkSound2;				//2 of 2 Audio clips to play when player collects a soda object.
 	public AudioClip gameOverSound;				//Audio clip to play when player dies.
-	
-	private Animator animator;					//Used to store a reference to the Player's animator component.
-	private Vector2 touchOrigin = -Vector2.one;	//Used to store location of screen touch origin for mobile controls.
-	
+
+	[SerializeField] private float maxSpeed = 2f;                    // The fastest the player can travel in the x axis.
+
+    const float ceilingRadius = .01f; // Radius of the overlap circle to determine if the player can stand up
+    private Animator animator;            // Reference to the player's animator component.
+    private bool facingRight = true;  // For determining which way the player is currently facing.
+
+    public Ray movingRay;
+    public Vector3 targetPosition;
+
+    private BoxCollider2D boxCollider2D;
 	
 	//Start overrides the Start function of MovingObject
 	void Start ()
 	{
 		//Get a component reference to the Player's animator component
 		animator = GetComponent<Animator>();
+		targetPosition = transform.position;
+		boxCollider2D = GetComponent<BoxCollider2D>();
 	}
 
 	private void Update ()
 	{
-		int horizontal = 0;  	//Used to store the horizontal move direction.
-		int vertical = 0;		//Used to store the vertical move direction.
-		
-		//Check if we are running either in the Unity editor or in a standalone build.
-		#if UNITY_STANDALONE || UNITY_WEBPLAYER
+		float horizontal = 0;  	//Used to store the horizontal move direction.
+		float vertical = 0;		//Used to store the vertical move direction.
 		
 		//Get input from the input manager, round it to an integer and store in horizontal to set x axis move direction
-		horizontal = (int) (Input.GetAxisRaw ("Horizontal"));
+		horizontal = Input.GetAxisRaw ("Horizontal");
 		
 		//Get input from the input manager, round it to an integer and store in vertical to set y axis move direction
-		vertical = (int) (Input.GetAxisRaw ("Vertical"));
-		
-		//Check if moving horizontally, if so set vertical to zero.
-		if(horizontal != 0)
-		{
-			vertical = 0;
-		}
-		
-		#endif //End of mobile platform dependendent compilation section started above with #elif
+		vertical = Input.GetAxisRaw ("Vertical");
+
 		//Check if we have a non-zero value for horizontal or vertical
-		if(horizontal != 0 || vertical != 0)
+		if (horizontal != 0 || vertical != 0)
 		{
-			// Add movemevent here
+			Move(horizontal, vertical);
 		}
 	}
 
 	//OnTriggerEnter2D is sent when another object enters a trigger collider attached to this object (2D physics only).
 	private void OnTriggerEnter2D (Collider2D other)
 	{
-		//Check if the tag of the trigger collided with is Exit.
-		if(other.tag == "Exit")
+		switch (other.tag)
 		{
-			//Invoke the Restart function to start the next level with a delay of restartLevelDelay (default 1 second).
-			Invoke ("Restart", restartLevelDelay);
-			
-			//Disable the player object since level is over.
-			enabled = false;
+			case "Next":
+				Debug.LogError("Next room");
+				break;
+			case "Wall":
+				Debug.LogError("Wall");
+
+//		        var charMotor = col.GetComponent(CharacterMotor);
+//		        charMotor.SetVelocity(targetPosition * velBack);
+				break;
+			default: 
+				
+				break;
 		}
 	}
-	
 	
 	//Restart reloads the scene when called.
 	private void Restart ()
 	{
 		//Load the last scene loaded, in this case Main, the only scene in the game.
-		Application.LoadLevel (Application.loadedLevel);
+		Application.LoadLevel(Application.loadedLevel);
 	}
+
+    private void Awake()
+    {
+        // Setting up references.
+        animator = GetComponent<Animator>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        // Set the vertical animation
+//		animator.SetFloat("vSpeed", Rigidbody2D.velocity.y);
+    }
+
+    public void Move(float hMove, float vMove)
+    {
+        // The Speed animator parameter is set to the absolute value of the horizontal input.
+//        animator.SetFloat("Speed", Mathf.Abs(move));
+
+		targetPosition = new Vector3(hMove, vMove, 0);
+
+		//Move Player
+		transform.Translate(targetPosition * maxSpeed * Time.deltaTime);
+
+        // If the input is moving the player right and the player is facing left...
+		if (hMove > 0 && !facingRight)
+        {
+            // ... flip the player.
+            Flip();
+        }
+
+            // Otherwise if the input is moving the player left and the player is facing right...
+		else if (hMove < 0 && facingRight)
+        {
+            // ... flip the player.
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        facingRight = !facingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    void OnDrawGizmos()
+    {
+		Gizmos.DrawLine(transform.position, transform.position + targetPosition);
+    }
 }
