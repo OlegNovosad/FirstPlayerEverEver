d6baa7e634026b73f71cde3cbe52a2cb56e46967
Oleg Novosad <onovosad@imprivata.com>
2016-02-23 16:49:47 +0200

    Added bat script to enable following player.

---
+++ Assets/Scripts/Bat.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bat : MonoBehaviour 
+{
+	private GameObject player;
+
+	private Vector3 velocity = Vector3.zero;
+
+	void Start()
+	{
+		player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		if (!GameManager.instance.isPaused)
+		{
+			transform.position = Vector3.SmoothDamp(transform.position, player.transform.position, ref velocity, 2f);
+		}
+	}
+}
